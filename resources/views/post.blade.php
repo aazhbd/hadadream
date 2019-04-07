cc27be0c4f302a122334ae9069c02681fb0d5d65
aazhbd <aazhbd@yahoo.com>
2019-04-07 18:15:14 +0200

    Updated initial setup for the site with template.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,21 @@
 @section('page-title'){{ $post->title }}@endsection
 
 @section('content')
-    <h1>{{ $post->title }}</h1>
-    <img src="{{ Voyager::image( $post->image ) }}" style="width:100%">
-    <p>{!! $post->body !!}</p>
+    <div class="card mb-4 rounded-corners">
+        <div class="card-body">
+            <h2 class="card-title"><a href="/dream/{{ $post->slug }}"><span>{{ $post->title }}</span></a></h2>
+            @if ($post->image)
+            <img src="{{ Voyager::image( $post->image ) }}" style="width:100%">
+            @endif
+            <p class="card-text">{!! $post->body !!}</p>
+            <a href="/dream/{{ $post->slug }}">alles anzeigen</a>
+            <table class="table table-sm text-muted my-0 mt-2">
+                <tr>
+                    <td class="px-0 py-0 pt-2">17 mal gelesen</td>
+                    <td class="px-0 py-0 pt-2 text-center">2 Kommentare</td>
+                    <td class="px-0 py-0 pt-2 text-right">{{ $post->created_at }}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 @endsection
